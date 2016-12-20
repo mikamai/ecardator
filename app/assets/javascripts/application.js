21c2b7b9c0f0ea511257447a0617b1d255b2f495
Matteo Revelli <matteo.revelli@mikamai.com>
2016-12-20 17:20:48 +0100

    add dynamic form for events

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,19 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+  var id = 1;
+  $("#addNewChoice").click(function(e){
+    e.preventDefault();
+    var html_add_title = "<div class=\"choiceForm\"><label for=\"Choice\">title "+id+"</label><br><input type=\"text\" name=\"ecard[title_event][]\" id=\"choices_title"+id+"\"> <a href=\"#\" class=\"remove-choice\"> X </a><br></div>";
+    var html_add_desc = "<div class=\"choiceForm\"><label for=\"Choice\">desc "+id+"</label><br><input type=\"text\" name=\"ecard[desc_event][]\" id=\"choices_desc"+id+"\"> <a href=\"#\" class=\"remove-choice\"> X </a><br></div>";
+    var html_add_link = "<div class=\"choiceForm\"><label for=\"Choice\">link "+id+"</label><br><input type=\"text\" name=\"ecard[link_event][]\" id=\"choices_"+id+"\"> <a href=\"#\" class=\"remove-choice\"> X </a><br></div>";
+    $("#choose_image").append(html_add_title + html_add_desc + html_add_link);
+    id = id +1;
+  });
+  $("#choose_image").on("click",".remove-choice",function(e){
+    e.preventDefault();
+    $(this).parent(".choiceForm").remove();
+  });
+});
