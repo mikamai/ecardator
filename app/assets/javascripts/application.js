0213523d6c733ad6b57a5ffceec09c32fe58274c
Matteo Revelli <matteo.revelli@mikamai.com>
2016-12-21 16:32:09 +0100

    add style and select color

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,10 +23,10 @@ $(document).ready(function(){
     var html_add_community = "<div class=\"choiceForm\"><label for=\"Choice\">Community "+id+"</label><a href=\"#\" class=\"remove-choice\"> X </a><input type=\"text\" name=\"ecard[community_event][]\" id=\"choices_community"+id+"\"></div>";
     var html_add_name_link = "<div class=\"choiceForm\"><label for=\"Choice\">Name_link "+id+"</label><a href=\"#\" class=\"remove-choice\"> X </a><br><input type=\"text\" name=\"ecard[name_link_event][]\" id=\"choices_link"+id+"\"></div>";
     var html_add_link = "<div class=\"choiceForm\"><label for=\"Choice\">Link "+id+"</label><a href=\"#\" class=\"remove-choice\"> X </a><br><input type=\"text\" name=\"ecard[link_event][]\" id=\"choices_link"+id+"\"></div>";
-    $("#choose_image").append(html_add_date + html_add_community + html_add_name_link + html_add_link);
+    $("#add_event").append(html_add_date + html_add_community + html_add_name_link + html_add_link);
     id = id +1;
   });
-  $("#choose_image").on("click",".remove-choice",function(e){
+  $("#add_event").on("click",".remove-choice",function(e){
     e.preventDefault();
     $(this).parent(".choiceForm").remove();
   });
